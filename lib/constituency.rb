a45dbb5e8ab246b44d0082b06289c46987f61c07
Ken Lim <kennethlimfc@gmail.com>
2013-05-06 22:11:14 +0100

    Added constituency data

--- lib/constituency.rb
+++ lib/constituency.rb
@@ -2,10 +2,12 @@ require 'rubygems'
 require 'nokogiri'
 
 class Constituency
-	attr_reader :candidates, :registeredVoters, :rejectedBallots, :countedBallots, :issuedBallots
+	attr_reader :candidates, :registeredVoters, :rejectedBallots, :countedBallots, :issuedBallots, :name, :state, :code
+	attr_reader :candidates, :registeredVoters, :rejectedBallots, :countedBallots, :issuedBallots, :name, :state, :code
 
 	def initialize(resultPage)
 		@page = Nokogiri::HTML(resultPage)
+		@state, @code, @name = grabConstituencyData()
 		@registeredVoters = grabBallotData("//tr[td[text() ='JUMLAH PEMILIH BERDAFTAR ']]//td[position() = 4]")
 		@rejectedBallots = grabBallotData("//tr[td[text() ='JUMLAH KERTAS UNDI DITOLAK ']]//td[position() = 4]")
 		@countedBallots = grabBallotData("//tr[td[text() ='JUMLAH KERTAS UNDI DALAM PETI UNDI ']]//td[position() = 4]")
@@ -15,4 +17,13 @@ class Constituency
 	def grabBallotData(xpath)
 		@page.at_xpath(xpath).inner_text().tr(",","").to_i
 	end
+
+	def grabConstituencyData()
+		wholeCell = @page.at_xpath("//td[@width='79%']/b").inner_html
+		first, blanko, last = wholeCell.split("<br>")
+		state = first.split(":").last.strip
+		code, name = last.split(":").last.split("-")
+		[state, code.strip, name.strip] 
+	end
+
 end
--- test/constituency_test.rb
+++ test/constituency_test.rb
@@ -6,9 +6,14 @@ class TestConstituencyPage < Test::Unit::TestCase
 		balikPulau = File.open("./test/balik_pulau.html")
 		results = Constituency.new(balikPulau)
 
-		assert_equal(49641, results.registeredVoters())
-		assert_equal(612, results.rejectedBallots())
-		assert_equal(43709, results.countedBallots())
-		assert_equal(43777, results.issuedBallots())
+		assert_equal("PULAU PINANG", results.state())
+		assert_equal("P.053", results.code)
+		assert_equal("BALIK PULAU", results.name)
+		assert_equal(49641, results.registeredVoters)
+		assert_equal(612, results.rejectedBallots)
+		assert_equal(43709, results.countedBallots)
+		assert_equal(43777, results.issuedBallots)
+
 	end
 end	
+
